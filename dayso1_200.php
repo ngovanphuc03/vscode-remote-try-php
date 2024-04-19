997af80ac17675fc4156002243f81f1733c5553b
ngovanphuc03 <96820000+ngovanphuc03@users.noreply.github.com>
2024-04-19 07:37:40 +0000

    ab

---
+++ dayso1_200.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+    for($i = 1;$i<=200;$i++)
+    {
+
+        if($i%2==0)
+
+            echo '<b><font color=red>'. $i . '</font></b>';
+
+        else
+
+            echo '<i><font color=blue>'. $i . '</font></i>';
+
+    }
+?>
+</body>
+</html>

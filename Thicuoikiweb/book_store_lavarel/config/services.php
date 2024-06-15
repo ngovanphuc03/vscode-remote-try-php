1aca2b0b725adccc0c8580272d60f308cf861764
nguyen duy tuan <124223016+hjdslaf@users.noreply.github.com>
2024-06-15 22:40:25 +0700

    aaaaa

---
+++ Thicuoikiweb/book_store_lavarel/config/services.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+    'google' => [
+        'client_id'     => '953825140740-cm126erd4jsm1mcn7n9a64seokku3hfi.apps.googleusercontent.com',
+        'client_secret' => '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',
+        'redirect'      => 'http://127.0.0.1:8000/account/google/callback',
+    ],
+
+];

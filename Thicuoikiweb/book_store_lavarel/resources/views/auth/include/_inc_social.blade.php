716f1a0612ce14afa40cdd0215f9d04151ee047d
nguyen duy tuan <124223016+hjdslaf@users.noreply.github.com>
2024-06-15 22:57:04 +0700

    Add files via upload

---
+++ Thicuoikiweb/book_store_lavarel/resources/views/auth/include/_inc_social.blade.php
@@ -0,0 +1,7 @@
+<hr>
+<a class="btn btn-google" href="{{ route('get.login.social',['social' => 'google']) }}">
+	<i class="fa fa-google-plus"></i> Google
+</a><br>
+{{-- <a class="btn btn-facebook" href="{{ route('get.login.social',['social' => 'facebook']) }}">
+	<i class="fa fa-facebook"></i> Facebook
+</a><br> --}}
